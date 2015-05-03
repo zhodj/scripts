8410c0924dd4383965eb8a45a114d9bcd8ae7389
zhodj <cs.zhoudj@gmail.com>
2015-05-03 22:52:01 +0800

    add main sub

--- perl/net_handler.pl
+++ perl/net_handler.pl
@@ -7,34 +7,45 @@ $password = "520fang";
 $user = "fangfang";
 $commnad_file = "cmd_list.txt";
 
-open(CMD_list, '<', $commnad_file) or die "Could not open file $commnad_file $!";
+sub romte_exec_cmd{
 
-my ($host,$pass) = ($IP,$password);
-my $exp = Expect->new;
-$exp = Expect->spawn("ssh -l $user $host");
-$exp->log_file("output.log", "w");
-$exp->expect(2,[
-        qr/password:/i,
-        sub {
-            my $self = shift ;
-            $self->send("$pass\n");
-            exp_continue;
-        }
-    ],
-    [
-        'connecting (yes/no)',
-        sub {
-            my $self = shift ;
-            $self->send("yes\n");
-        }
-    ]
-);
+    open(CMD_list, '<', $commnad_file) or die "Could not open file $commnad_file $!";
 
-foreach $cmd (<CMD_list>){
-    chomp($cmd);
-    $exp->send("$cmd\n") if ($exp->expect(undef,'~'));
+    my ($host,$pass) = ($IP,$password);
+    my $exp = Expect->new;
+    $exp = Expect->spawn("ssh -l $user $host");
+    $exp->log_file("output.log", "w");
+    $exp->expect(2,[
+            qr/password:/i,
+            sub {
+                my $self = shift ;
+                $self->send("$pass\n");
+                exp_continue;
+            }
+        ],
+        [
+            'connecting (yes/no)',
+            sub {
+                my $self = shift ;
+                $self->send("yes\n");
+            }
+        ]
+    );
+
+    foreach $cmd (<CMD_list>){
+        chomp($cmd);
+        $exp->send("$cmd\n") if ($exp->expect(undef,'~'));
+    }
+    $exp->send("exit\n") if ($exp->expect(undef,'~'));
+    $exp->log_file(undef);
+    $exp->interact() if ($exp->expect(undef,'~'));
+    close ($CMD_list);
+
 }
-$exp->send("exit\n") if ($exp->expect(undef,'~'));
-$exp->log_file(undef);
-$exp->interact() if ($exp->expect(undef,'~'));
-close ($CMD_list);
+
+sub main
+{
+    romte_exec_cmd();
+}
+
+&main(@ARGV);
